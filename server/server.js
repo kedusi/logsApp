366e1a4a9606904e28688b8ac037c6fad78e33e0
Kennon <kedusi@gmail.com>
2022-03-10 15:18:27 -0600

    server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config({ path: './config/.env' });
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+app.use(require('./routes/record'));
+
+const dbo = require('./db/conn');
+
+app.listen(port, () => {
+    dbo.connectToServer(function(err) {
+        if(err) console.error(err);
+    });
+    console.log(`Server is running on port ${port}`);
+});
